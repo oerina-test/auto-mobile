c0b10e8ac92f7a2572e4def1f11b0e93be97c73a
oerina-test <73638008+oerina-test@users.noreply.github.com>
2020-10-29 16:06:29 +0700

    Тест 1 Урок 1

---
+++ JavaAppiumAutomation/src/MainClass.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class MainClass
+
+{
+    int a;
+    @Test
+
+    public void getLocalNumber(){
+
+        a = 14;
+
+    }
+}
---
+++ JavaAppiumAutomation/src/MainClassTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+public class MainClassTest extends MainClass
+{
+    @Test
+
+    public void testGetLocalNumber(){
+        this.getLocalNumber();
+        if (a==14) {
+            System.out.print("Тест работает");
+        }
+        else {
+            System.out.print("Тест не работает");
+        }
+    }
+}
