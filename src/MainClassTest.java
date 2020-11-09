be5dd4a27682e7b7ba21137a3d35217a075b097e
oerina-test <73638008+oerina-test@users.noreply.github.com>
2020-11-09 09:52:04 +0700

    Урок 1 Тест 3

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,24 +1,23 @@
 import org.junit.Test;
 
-public class MainClassTest extends MainClass
-{
-@Test
-    public void testGetLocalNumber () {
-    int res = this.getLocalNumber();
-    assert res == 14;
-}
-
-@Test
-    public void testGetClassNumber (){
-    assert this.getClassNumber() > 45: "Все верно";
-}
-
-
-@Test
-    public void  testGetClassString (){
-        String h1 = "Hello";
-    assert this.getClassString().toLowerCase().contains(h1.toLowerCase());
+public class MainClassTest extends MainClass {
+    @Test
+    public void testGetLocalNumber() {
+        int res = this.getLocalNumber();
+        assert res == 14;
     }
 
+    @Test
+    public void testGetClassNumber() {
+        assert this.getClassNumber() > 45 : "Все верно";
     }
 
+
+    @Test
+    public void testGetClassString() {
+        //  String h1 = "Hello";
+        //assert this.getClassString().toLowerCase().contains(h1.toLowerCase());
+        // assert this.getClassString().contains("Hello");
+        assert this.getClassString().contains("Hello")| this.getClassString().contains("hello");
+    }
+}
